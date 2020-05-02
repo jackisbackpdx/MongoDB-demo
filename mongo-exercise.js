ba896eb9ecf83bda91de9c53c62f83aa2272349e
Jackson <jackson.kelley.g@gmail.com>
2020-05-02 01:20:37 -0700

    using mongoose to delete and change mongodb documents

--- mongo-exercise.js
+++ mongo-exercise.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/mongo-exercises')
+mongoose.connect('mongodb://localhost/mongo-exercise', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(console.log('Connected to database'))
     .catch(err => console.log('Error: ', err));
 
@@ -17,9 +17,14 @@ const Course = mongoose.model('Course', courseSchema);
 
 async function getCourses() {
     return await Course
-        .find({ isPublished: true, tags: { $in: ['frontend', 'backend'] } })
-        .sort({ price: -1 })
-        .select({ name: 1, author: 1 });
+        // .find({ isPublished: true, tags: { $in: ['frontend', 'backend'] } })
+        .find({ isPublished: true })
+        .or([
+            { price: { $gte: 15 } },
+            { name: /.*by.*/i }
+        ])
+        .sort({ price: 1 })
+        .select({ name: 1, author: 1, price: 1, tags: 1 });
 }
 
 async function run() {
@@ -27,4 +32,21 @@ async function run() {
     console.log(courses);
 }
 
-run();
+
+async function updateCourse(id) {
+    const course = await Course.findByIdAndUpdate(id, {
+        $set: {
+            author: 'Jackson Kelley',
+            isPublished: true,
+            price: 10000
+        }
+    }, { new: true });
+    console.log(course);
+}  
+
+async function removeCourse(id) {
+    // const result = await Course.deleteMany({  });
+    const course = await Course.findByIdAndRemove(id);
+    console.log(course);
+}
+removeCourse('5eacdbe29c8cf057674ea400');
